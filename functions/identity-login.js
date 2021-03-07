8a82aaf6c730397fda9dd3146faff6f392519286
Cynthia Tsoi <cynthia@digitalchaos.ca>
2021-03-07 16:30:58 -0500

    yikes

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -3,12 +3,13 @@ const {CREATE_USER} = require("./queries/user")
 const formatResponse = require("./utils/formatResponse")
 
 exports.handler = async (event, context) => {
-    const {user} = context.clientContext
-    try {
-        const data = await sendQuery(CREATE_USER, {identifier: user.id})
-        return formatResponse(200, data)
-    } catch (e) {
-        console.log(e)
-        return formatResponse(500, {message: "something went wrong, too bad!"})
-    }
+    return formatResponse(200, {message:"keep going"})
+    // const {user} = context.clientContext
+    // try {
+    //     const data = await sendQuery(CREATE_USER, {identifier: user.id})
+    //     return formatResponse(200, data)
+    // } catch (e) {
+    //     console.log(e)
+    //     return formatResponse(500, {message: "something went wrong, too bad!"})
+    // }
 }
